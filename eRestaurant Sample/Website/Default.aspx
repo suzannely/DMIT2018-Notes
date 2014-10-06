ac76a1b4fc7bf4958c54929b20c03e4223d25122
suzannetruong <suzanne_@live.ca>
2014-10-06 16:54:13 -0600

    eRestaurant

---
+++ eRestaurant Sample/Website/Default.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        <h1>Welcome to eRestaurant</h1>
+    </div>
+</asp:Content>
---
+++ eRestaurant Sample/eRestaurant/Entities/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRestaurant.Entities
+{
+    public class Employee
+    {
+        public Employee() 
+        {
+            Active = true;
+        }
+
+        public int EmployeeID { get; set; }
+        public string FirstName {get; set;}
+        public string LastName {get; set;}
+        public char HomePhone {get; set;}
+        public bool Active { get; set; }
+    }
+  
+}
